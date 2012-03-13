7bf55b3766959f195982900edc5b6469604b3fe2
Marco Antonini <marcomail.anto@gmail.com>
2012-03-13 12:56:05 +0100

    Added Comment to BlackBox example

--- BlackBox/BlackBox.ino
+++ BlackBox/BlackBox.ino
@@ -5,34 +5,32 @@
 #include "TimerOne.h"
 
 #define DUMP_INTERVAL 200 // Store interval [ms]
-#define SD_CHIPSEL 10
-#define FILE "DUMP.CSV"
-#define STATE_LED 9
-#define BUTTON 6
+#define SD_CHIPSEL 10     // SD card chip select
+#define FILE "DUMP.CSV"   // Dump file name
+#define STATE_LED 9       // Led dump indicator
+#define BUTTON 6          // pin button Start-Stop dump 
 
 Daisy7 imu; // Daisy7 object
 
-volatile boolean gps_ready = false; // GPS decode status
-boolean dump_active = false;
+volatile boolean gps_ready = false;  // GPS decode status
+boolean dump_active = false;         // Start-Stop Dump to SD
 
 void setup()
 {
   pinMode(STATE_LED, INPUT);
   pinMode(BUTTON, INPUT);
   
-  Serial.begin(115200);
-  gps_setup();
+  Serial.begin(115200);  // GPS Baud-rate : default 115200
+  gps_setup();           // init gps
 
-  // Daisy7 IMU begin
-  imu.begin();
+  imu.begin();           // Daisy7 IMU begin
 
-  imu.MagnSetScale(1.3); // Set the scale of the imu.
-  imu.MagnSetMeasurementMode(Measurement_Continuous); // Set the measurement mode to Continuous
+  imu.MagnSetScale(1.3); // Set the scale of the magnetometer.
+  imu.MagnSetMeasurementMode(Measurement_Continuous); // Set MAgnetometer measurement mode to Continuous
 
-  // SD card begin
-  if (SD.begin(SD_CHIPSEL))
+  if (SD.begin(SD_CHIPSEL))   // SD card begin
   {
-    if (digitalRead(BUTTON) == LOW)
+    if (digitalRead(BUTTON) == LOW)  // Reset dump file when Button is pressed at boot
     {
       SD.remove(FILE);
       pinMode(STATE_LED, OUTPUT);
@@ -48,24 +46,27 @@ void setup()
     // Open File
     File dataFile;
     
-    // CSV Header
-    if (!SD.exists(FILE))
+    if (!SD.exists(FILE))  // Create CSV Header
     {
       dataFile = SD.open(FILE, FILE_WRITE);
+      dataFile.println(F("BEE Board BlackBox Dump CSV"));
+      dataFile.print(F("Dump Interval: "));
+      dataFile.print(String(DUMP_INTERVAL));
+      dataFile.println(" ms");
       dataFile.println(F("GPS-Time,GPS-Latitude,GPS-Longitude,GPS-Course,GPS-Speed,GPS-Altitude,Magn-X,Magn-Y,Magn-Z,Acc-X,Acc-Y,Acc-Z,Gyro-X,Gyro-Y,Gyro-Z,Temperature,Pressure,Altitude"));
       dataFile.close();
     }
   }
   
-  // TimerOne init
-  Timer1.initialize(1000); // 1000 us
-  Timer1.attachInterrupt(gpsEvent);
+  // Timer GPS Event configuration 
+  Timer1.initialize(1000);           // 1000 us
+  Timer1.attachInterrupt(gpsEvent);  // GPS Event Interrupt
 }
 
 
 void loop()
 {
-  if (digitalRead(BUTTON) == LOW)
+  if (digitalRead(BUTTON) == LOW) // Start-Stop Dump
   {
     dump_active = !dump_active;
     delay(1000);
@@ -81,46 +82,49 @@ void loop()
      
     if (dataFile)
     {
-      if (gps_ready)
+      /*************  GPS Data   **************/
+      if (gps_ready) // if GPS data are valid
       {
-        // GPS Time
+        // Append GPS Time
         String time = String(gps_time);
         data += time;
         data += ",";
         
-        // GPS Latitude
+        // Append GPS Latitude
         dtostrf(gps_lat, 4, 4, buff);
         data += buff;
         data += ",";
         
-        // GPS Longitude
+        // Append GPS Longitude
         dtostrf(gps_lon, 4, 4, buff);
         data += buff;
         data += ",";
         
-        // GPS Course
+        // Append GPS Course
         dtostrf(gps_course, 4, 4, buff);
         data += buff;
         data += ",";
         
-        // GPS Speed
+        // Append GPS Speed
         dtostrf(gps_speed, 4, 4, buff);
         data += buff;
         data += ",";
         
-        // GPS Altitude
+        // Append GPS Altitude
         dtostrf(gps_altitude, 4, 4, buff);
         data += buff;
         data += ",";
         
-        // Store GPS data
+        // Store GPS data to CSV
         dataFile.print(data);
         data = "";
         
         gps_ready = false;
       }
-      else
+      else 
         dataFile.print(",,,,,,");
+     
+     /*************  Magnetometer Data   **************/
      
       // Retrieve the Raw Values from the Magnetometer.
       MagnetometerRaw magn = imu.MagnReadRawAxis();
@@ -140,79 +144,85 @@ void loop()
       data += buff;
       data += ",";
       
-      // Store Magnetometer data
+      // Store Magnetometer data to CSV
       dataFile.print(data);
       data = "";
   
+      /*************  Accelerometer Data   **************/
+      
       // Retrieve the Raw Values from the Accelerometer.
       AccelerometerRaw acc;
       acc = imu.Accelerometer();
       
-      // Accelerometer Raw X
+      // Append Accelerometer Raw X
       dtostrf(acc.XAxis, 4, 4, buff);
       data += buff;
       data += ",";
       
-      // Accelerometer Raw Y
+      // Append Accelerometer Raw Y
       dtostrf(acc.YAxis, 4, 4, buff);
       data += buff;
       data += ",";
       
-      // Accelerometer Raw Z
+      // Append Accelerometer Raw Z
       dtostrf(acc.ZAxis, 4, 4, buff);
       data += buff;
       data += ",";
-   
-      // Store Accelerometer data
+      
+      // Store Accelerometer data to CSV
       dataFile.print(data);
       data = "";
-  
+   
+      /*************  Gyroscope Data   **************/
+        
       // Retrieve the Raw Values from the Gyroscope.
       GyroRaw gyro;
       gyro = imu.GyroRead();
       
-      // Gyroscope Raw X
+      // Append Gyroscope Raw X
       dtostrf(gyro.XAxis, 4, 4, buff);
       data += buff;
       data += ",";
       
-      // Gyroscope Raw Y
+      // Append Gyroscope Raw Y
       dtostrf(gyro.YAxis, 4, 4, buff);
       data += buff;
       data += ",";
       
-      // Gyroscope Raw Z
+      // Append Gyroscope Raw Z
       dtostrf(gyro.ZAxis, 4, 4, buff);
       data += buff;
       data += ",";
       
-      // Store Gyroscope data
+      // Store Gyroscope data to CSV
       dataFile.print(data);
       data = "";
+      
+      /*************  Barometer Data   **************/
   
       // Retrieve the Values from the Barometer.
       float temp = imu.BaroGetTemperature(imu.bmp085ReadUT());
       float pressure = imu.BaroGetPressure(imu.bmp085ReadUP());
       float altitude = imu.calcAltitude(pressure);
       
-      // Barometer Temperature
+      // Append Barometer Temperature
       dtostrf(temp, 4, 4, buff);
       data += buff;
       data += ",";
       
-      // Barometer Pressure
+      // Append Barometer Pressure
       dtostrf(pressure/100, 4, 4, buff);
       data += buff;
       data += ",";
       
-      // Barometer Altitude
+      // Append Barometer Altitude
       dtostrf(altitude, 4, 4, buff);
       data += buff;
       
-      // Store Barometer data
+      // Store Barometer data to CSV
       dataFile.println(data);
       
-      // Led On
+      // Flash led to each write
       pinMode(STATE_LED, OUTPUT);
     }
     
@@ -226,11 +236,12 @@ void loop()
 }
 
 
+/*********  GPS Event Interrupt   *********/
 void gpsEvent() {
   int val;
-  while (Serial.available()) {
+  while (Serial.available()) { // Empties the buffer of the Serial
     val = Serial.read();
-    if (gps_decode(val))
+    if (gps_decode(val)) // decode NMEA GPS data
       gps_ready = true;
   }
 }
